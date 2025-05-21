fee9aade53ae917260317ef486eed6d3dc8a7863
Park, Yeonji <yeonpyj23@gmail.com>
2025-05-21 09:31:48 +0900

    five

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("박연지");
         World.run();
+        World.run2();
     }
 }
--- src/World.java
+++ src/World.java
@@ -3,4 +3,8 @@ public class World {
     public static void run() {
         System.out.println("202314044");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
